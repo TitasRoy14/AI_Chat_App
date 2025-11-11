a377d9fd45b1d0f499476261eef1cdbfa3bd57cf
TitasRoy14 <roytitas800@gmail.com>
2025-11-11 13:55:30 +0530

    Refactor to chat controller

---
+++ packages/server/controllers/chat.controllers.ts
@@ -0,0 +1,33 @@
+import type { Request, Response } from "express";
+import z from "zod";
+import { chatService } from "../services/chat.services";
+
+const chatSchema = z.object({
+  prompt: z
+    .string()
+    .trim()
+    .min(1, "Prompt is required")
+    .max(1000, "Prompt is too long"),
+  conversationId: z.string().pipe(z.string().uuid()),
+});
+
+export const chatController = {
+  async sendMessage(req: Request, res: Response) {
+    const parseResult = chatSchema.safeParse(req.body);
+
+    if (!parseResult.success) {
+      res.status(400).json(parseResult.error.format());
+      return;
+    }
+
+    try {
+      const { prompt, conversationId } = req.body;
+
+      const { message } = await chatService.sendMessage(prompt, conversationId);
+
+      res.json({ message: message });
+    } catch (error) {
+      res.send(500).json({ error: "Failed to generate a response" });
+    }
+  },
+};
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,5 @@
 import express, { type Request, type Response } from "express";
-import { z } from "zod";
-import { chatService } from "./services/chat.services";
+import { chatController } from "./controllers/chat.controllers";
 
 const app = express();
 app.use(express.json());
@@ -13,33 +12,7 @@ app.get("/api/hello", (req: Request, res: Response) => {
   res.json({ message: "Here is json" });
 });
 
-const chatSchema = z.object({
-  prompt: z
-    .string()
-    .trim()
-    .min(1, "Prompt is required")
-    .max(1000, "Prompt is too long"),
-  conversationId: z.string().pipe(z.string().uuid()),
-});
-
-app.post("/api/chat", async (req: Request, res: Response) => {
-  const parseResult = chatSchema.safeParse(req.body);
-
-  if (!parseResult.success) {
-    res.status(400).json(parseResult.error.format());
-    return;
-  }
-
-  try {
-    const { prompt, conversationId } = req.body;
-
-    const { message } = await chatService.sendMessage(prompt, conversationId);
-
-    res.json({ message: message });
-  } catch (error) {
-    res.send(500).json({ error: "Failed to generate a response" });
-  }
-});
+app.post("/api/chat", chatController.sendMessage);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
